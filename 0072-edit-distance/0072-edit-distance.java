04b8ed7af9f2f21ade555266724ead8517b4d2b0
Soumen Mondal <117503345+s479mondal@users.noreply.github.com>
2026-07-23 22:43:09 +0530

    Time: 5 ms (67.53%), Space: 47 MB (84.29%) - LeetHub

--- 0072-edit-distance/0072-edit-distance.java
+++ 0072-edit-distance/0072-edit-distance.java
@@ -1,23 +1,25 @@
 class Solution {
     public int minDistance(String word1, String word2) {
-        int m=word1.length();
-        int n=word2.length();
-        int[][] a=new int[m+1][n+1];
-        for(int i=0;i<=n;i++)
-        {
-            a[0][i]=i;
-        }
-        for(int i=0;i<=m;i++)
+        int n1=word1.length();
+        int n2=word2.length();
+        int[][] a=new int[n1+1][n2+1];
+        int min=Integer.MAX_VALUE;
+        for(int i=1;i<=n1;i++)
         {
             a[i][0]=i;
         }
-        for(int i=1;i<=m;i++)
+        for(int i=1;i<=n2;i++)
         {
-            for(int j=1;j<=n;j++)
+            a[0][i]=i;
+        }
+        a[0][0]=0;
+        for(int i=1;i<=n1;i++)
+        {
+            for(int j=1;j<=n2;j++)
             {
                 if(word1.charAt(i-1)!=word2.charAt(j-1))
                 {
-                    a[i][j]=1+Math.min(a[i-1][j],Math.min(a[i][j-1],a[i-1][j-1]));
+                    a[i][j]=Math.min(a[i-1][j-1],Math.min(a[i-1][j],a[i][j-1]))+1;
                 }
                 else
                 {
@@ -25,6 +27,6 @@ class Solution {
                 }
             }
         }
-        return a[m][n];
+        return a[n1][n2];
     }
 }
