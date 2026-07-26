bcaab8a21848dc60e5ad30c65c0c4e3a1581b217
Soumen Mondal <117503345+s479mondal@users.noreply.github.com>
2026-07-26 15:04:00 +0530

    Time: 505 ms (51.52%), Space: 0B (100%) - LeetHub

--- 0197-rising-temperature/0197-rising-temperature.sql
+++ 0197-rising-temperature/0197-rising-temperature.sql
@@ -1,1 +1,1 @@
-select w1.id from weather as w1 join weather as w2 on datediff(w1.recorddate,w2.recorddate)=1 where w1.temperature>w2.temperature;
+select w1.id from Weather as w1 cross join Weather as w2 where datediff(w1.recorddate,w2.recorddate)=1 and w1.temperature>w2.temperature;
