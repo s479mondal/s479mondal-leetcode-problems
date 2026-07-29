6b2dde066f7b3ca29c9bd8c349d112e0e3a1eeee
Soumen Mondal <117503345+s479mondal@users.noreply.github.com>
2026-07-29 11:09:23 +0530

    Time: 2 ms (88.85%), Space: 47.9 MB (62.97%) - LeetHub

--- 0055-jump-game/0055-jump-game.java
+++ 0055-jump-game/0055-jump-game.java
@@ -1,9 +1,13 @@
 class Solution {
     public boolean canJump(int[] nums) {
-        int cj=nums[0];
-        for(int i=1;i<nums.length;i++){
-            if(i>cj) return false;
-            cj=Math.max(cj,nums[i]+i);
+        int n=nums[0];
+        for(int i=1;i<nums.length;i++)
+        {
+            if(i>n)
+            {
+                return false;
+            }
+            n=Math.max(n,nums[i]+i);
         }
         return true;
     }
