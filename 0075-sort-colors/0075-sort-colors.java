c9ea6aabed40b61688537f169a857d41776a9311
Soumen Mondal <117503345+s479mondal@users.noreply.github.com>
2026-08-02 22:32:10 +0530

    Time: 0 ms (100%), Space: 43.4 MB (61.68%) - LeetHub

---
+++ 0075-sort-colors/0075-sort-colors.java
@@ -0,0 +1,30 @@
+class Solution {
+    public void sortColors(int[] nums) {
+        int low=0;
+        int mid=0;
+        int high=nums.length-1;
+        int t=0;
+        while(mid<=high)
+        {
+            if(nums[mid]==0)
+            {
+                t=nums[low];
+                nums[low]=nums[mid];
+                nums[mid]=t;
+                low++;
+                mid++;
+            }   
+            else if(nums[mid]==1)
+            {
+                mid++;
+            }
+            else
+            {
+                t=nums[mid];
+                nums[mid]=nums[high];
+                nums[high]=t;
+                high--;
+            }
+        }
+    }
+}
