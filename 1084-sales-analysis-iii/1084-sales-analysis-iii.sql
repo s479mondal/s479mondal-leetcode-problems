ca128f33feb94aa37d6c1d8e215a5dcfad69d4ff
Soumen Mondal <117503345+s479mondal@users.noreply.github.com>
2026-08-23 13:50:37 +0530

    Time: 1474 ms (20.34%), Space: 0B (100%) - LeetHub

---
+++ 1084-sales-analysis-iii/1084-sales-analysis-iii.sql
@@ -0,0 +1,1 @@
+select p.product_id,p.product_name from product as p join sales as s on p.product_id=s.product_id group by p.product_id,p.product_name having min(s.sale_date)>="2019-01-01" and max(s.sale_date)<="2019-03-31";
